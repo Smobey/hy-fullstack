7e19f04ff50acee3aaa65fd20a8e7d9066b5d6ab
Topi T <smobey@gmail.com>
2018-01-21 18:43:54 +0200

    2.4

--- Osa 2/osa2/src/index.js
+++ Osa 2/osa2/src/index.js
@@ -20,31 +20,58 @@ const Kurssi = (props) => {
     )
 }
 
+const Kurssit = (props) => {
+    return (
+        <div>
+            {props.kurssit.map(kurssi=><Kurssi key={kurssi.id} kurssi={kurssi}/>)}
+        </div>
+    )
+}
+
 const App = () => {
-    const kurssi = {
-      nimi: 'Half Stack -sovelluskehitys',
-      osat: [
+    const kurssit = [
         {
-          nimi: 'Reactin perusteet',
-          tehtavia: 10,
-          id: 1
+          nimi: 'Half Stack -sovelluskehitys',
+          id: 1,
+          osat: [
+            {
+              nimi: 'Reactin perusteet',
+              tehtavia: 10,
+              id: 1
+            },
+            {
+              nimi: 'Tiedonvälitys propseilla',
+              tehtavia: 7,
+              id: 2
+            },
+            {
+              nimi: 'Komponenttien tila',
+              tehtavia: 14,
+              id: 3
+            }
+          ]
         },
         {
-          nimi: 'Tiedonvälitys propseilla',
-          tehtavia: 7,
-          id: 2
-        },
-        {
-          nimi: 'Komponenttien tila',
-          tehtavia: 14,
-          id: 3
+          nimi: 'Node.js',
+          id: 2,
+          osat: [
+            {
+              nimi: 'Routing',
+              tehtavia: 3,
+              id: 1
+            },
+            {
+              nimi: 'Middlewaret',
+              tehtavia: 7,
+              id: 2
+            }
+          ]
         }
       ]
-    }
   
     return (
       <div>
-        <Kurssi kurssi={kurssi} />
+        <Kurssit kurssit={kurssit} />
       </div>
     )
   }
